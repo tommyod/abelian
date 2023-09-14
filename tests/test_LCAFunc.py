833730c691e879196c8418bc1fb784dc7b755c43
vasco <vasco.schiavo@protonmail.com>
2023-09-14 08:32:37 +0200

    update test LCAfunc

--- tests/test_LCAFunc.py
+++ tests/test_LCAFunc.py
@@ -1,34 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import random
-from abelian import LCA, LCAFunc, HomLCA, voronoi
+from math import isclose
 
+import pytest
 
-def is_close(list1, list2):
-    return sum(abs(i-j) for (i,j) in zip(list1, list2)) < 10e-10
+from abelian import LCA, HomLCA, voronoi
+
 
 class TestLCAFunc:
 
-    def test_natural_transversal_real(self):
-        """
-        Test transversal from T^2 to R^2.
-        """
-
+    @pytest.fixture
+    def sigma(self):
         T = LCA([1], [False])
         R = LCA([0], [False])
         A = [[0, 1], [1, -0.5]]
+
         epimorphism = HomLCA(A, source=R ** 2, target=T ** 2)
         sigma = voronoi(epimorphism, 2)
 
-        assert is_close(sigma([0,0]), [0,0])
-        assert is_close(sigma([0.8, 0.8]), [-0.3, -0.2])
-        assert is_close(sigma([0.4, 0.8]), [0, 0.4])
-        assert is_close(sigma([0.8, 0.4]), [0.3, -0.2])
+        return sigma
 
+    @pytest.mark.parametrize(
+        'input_list,  output_list',
+        [
+            ([0, 0], [0, 0]),
+            ([0.8, 0.8], [-0.3, -0.2]),
+            ([0.4, 0.8], [0, 0.4]),
+            ([0.8, 0.4], [0.3, -0.2])
+        ]
+    )
+    def test_natural_transversal_real(self, input_list, output_list, sigma):
+        """ Test transversal from T^2 to R^2 """
 
-
-
-if __name__ == '__main__':
-    tests = TestLCAFunc()
-    tests.test_natural_transversal_real()
+        for input_value, excepted_value in zip(sigma(input_list), output_list):
+            assert isclose(input_value, excepted_value, abs_tol=10e-10)
