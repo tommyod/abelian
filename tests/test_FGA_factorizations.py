7c6ec5f2b8d97325b8a15cec605f51f361757fc4
vasco <vasco.schiavo@protonmail.com>
2023-09-14 08:32:08 +0200

    update test FGA factorizations

--- tests/test_FGA_factorizations.py
+++ tests/test_FGA_factorizations.py
@@ -1,52 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import random
-from sympy import Matrix
 from random import randint as ri
-from abelian.morphisms import HomLCA
 
-def random_zero_heavy(low, high):
-    """
-    Draw a random number, with approx 50% probability of zero.
-    """
-    return random.choice(list(range(low, high)) + [0]*(high - low))
+import pytest
+from sympy import Matrix
+
+from abelian.morphisms import HomLCA
+from utils import random_zero_heavy
+
 
 class TestSNF:
 
-    @staticmethod
-    def setup():
-        """
-        Setup a homomorphism.
-        """
-
-        # --------- START SETUP ---------
+    @pytest.fixture
+    def homomorphism(self):
+        """ Set up a homomorphism """
         m, n = ri(1, 5), ri(1, 5)
         A = Matrix(m, n, lambda i, j: random_zero_heavy(-9, 9))
         target = Matrix(m, 1, lambda i, j: random_zero_heavy(5, 50))
         target_vector = target
-        phi = HomLCA(A, target = target)
-        # --------- END SETUP ---------
+        phi = HomLCA(A, target=target)
         return phi, target_vector
 
-
-    def test_source_projection_two_ways(self):
-        """
-        Test that phi.project to source == phi.coimage.project_to_source
-        """
-        phi, target_vector = self.setup()
+    def test_source_projection_two_ways(self, homomorphism):
+        """ Test that phi.project_to_source == phi.coimage.project_to_source """
+        phi, target_vector = homomorphism
 
         # Project to source
         phi = phi.project_to_source()
         coimage = phi.coimage().project_to_source()
+
         assert phi.source == coimage.source
 
-
-    def test_project_to_source(self):
-        """
-        Test the orders.
-        """
-        phi, target_vector = self.setup()
+    def test_project_to_source(self, homomorphism):
+        """ Test the orders """
+        phi, target_vector = homomorphism
 
         # Project to source
         phi = phi.project_to_source()
@@ -57,12 +45,9 @@ class TestSNF:
         # Assert that the columns times the orders are zero
         assert phi.evaluate(orders) == Matrix(phi.target.orders) * 0
 
-
-    def test_image_coimage_factorization(self):
-        """
-        Test the image/coimage factorization.
-        """
-        phi, target_vector = self.setup()
+    def test_image_coimage_factorization(self, homomorphism):
+        """ Test the image/coimage factorization """
+        phi, target_vector = homomorphism
 
         # Compute the image morphism and the coimage morphism
         image = phi.image().remove_trivial_groups()
@@ -70,32 +55,27 @@ class TestSNF:
 
         # Asser that the coimage/image factorization holds
         factorization = (image * coimage).project_to_target()
-        original = (phi).project_to_target()
+        original = phi.project_to_target()
+
         assert factorization == original
 
-
-    def test_kernel(self):
-        """
-        The that the kernel and the morphism is zero.
-        """
-        phi, target_vector = self.setup()
+    def test_kernel(self, homomorphism):
+        """ The that the kernel and the morphism is zero """
+        phi, target_vector = homomorphism
 
         # Compute the kernel
         kernel = phi.kernel()
         phi_ker = (phi * kernel).project_to_target()
-        zero_morphism = HomLCA.zero(target = phi.target,
-                                    source = kernel.source)
+        zero_morphism = HomLCA.zero(target=phi.target, source=kernel.source)
+
         assert phi_ker == zero_morphism
 
-
-    def test_cokernel(self):
-        """
-        Test that the composition of the cokernel and the morphism is zero.
-        """
-        phi, target_vector = self.setup()
+    def test_cokernel(self, homomorphism):
+        """ Test that the composition of the cokernel and the morphism is zero """
+        phi, target_vector = homomorphism
 
         cokernel = phi.cokernel()
-        coker_phi = (cokernel * phi).project_to_target()
-        zero_morphism = HomLCA.zero(source = phi.source,
-                                    target = cokernel.target)
-        assert coker_phi == zero_morphism
+        cokernel_phi = (cokernel * phi).project_to_target()
+        zero_morphism = HomLCA.zero(source=phi.source, target=cokernel.target)
+
+        assert cokernel_phi == zero_morphism
